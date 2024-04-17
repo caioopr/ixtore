5c04938455b83e3d84ad87ea05b2adfd36222a67
Caio Prado <caioppubli@gmail.com>
2024-04-16 22:49:11 -0300

    base app

---
+++ src/main/java/com/caioop/ixtore/IxtoreApplication.java
@@ -0,0 +1,13 @@
+package com.caioop.ixtore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IxtoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IxtoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caioop/ixtore/IxtoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caioop.ixtore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IxtoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
