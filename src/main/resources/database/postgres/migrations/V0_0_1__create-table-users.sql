bdcafc034567156ba6ae17cd1beef0768a5f2c1e
Caio Prado <caioppubli@gmail.com>
2024-04-21 16:53:14 -0300

    Docker compose postgreSQL database

--- src/main/resources/database/postgres/migrations/V0_0_1__create-table-users.sql
+++ src/main/resources/database/postgres/migrations/V0_0_1__create-table-users.sql
@@ -1,7 +1,6 @@
 CREATE TABLE tb_users (
     user_id SERIAL PRIMARY KEY,
-    --user_uuid uuid DEFAULT gen_random_uuid() UNIQUE,
-     user_uuid uuid DEFAULT random_uuid() UNIQUE,
+    user_uuid uuid DEFAULT gen_random_uuid() UNIQUE,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(50) NOT NULL,
