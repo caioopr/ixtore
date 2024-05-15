b1bba97a8a78f3cb24b4f5ce34bef473fd73ae40
Caio Prado <caioppubli@gmail.com>
2024-05-14 21:25:53 -0300

    Client and Auth Form UI

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <router-view></router-view>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  height: 100%;
+}
+</style>
---
+++ client/src/router/index.js
@@ -0,0 +1,63 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/sales',
+    },
+    {
+      path: '/login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Users/LoginUserView.vue'),
+    },
+    {
+      path: '/register',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Users/RegisterUserView.vue'),
+    },
+    {
+      path: '/current_user',
+      component: () => import('../views/Users/CurrentUserView.vue'),
+    },
+    {
+      path: '/products',
+      component: null, //() => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/products/:id',
+      component: null, //() => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/customers',
+      component: null, //() => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/customers/:id',
+      component: null, //() => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/sales',
+      component: null, //() => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/sales/:id',
+      component: null, //() => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/dashboard',
+      component: null, //() => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/:notFound(.*)',
+      component: null, //() => import('../views/AboutView.vue'),
+    },
+  ],
+});
+
+export default router;
