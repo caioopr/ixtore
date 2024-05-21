39dccb82f5202f14be946ed6de9000f04d10978d
Caio Prado <caioppubli@gmail.com>
2024-05-21 15:39:08 -0300

    products table

---
+++ server/src/main/resources/database/postgres/migrations/V0_0_2__create-table-products.sql
@@ -0,0 +1,11 @@
+CREATE TABLE tb_products (
+    product_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    price NUMERIC(16,4) NOT NULL, -- 000.000.000.000,0000
+    description VARCHAR(500),
+    weight DOUBLE PRECISION,
+    fk_user_uuid uuid,
+    supplier VARCHAR(100),
+    CONSTRAINT fk_user FOREIGN KEY (fk_user_uuid) REFERENCES tb_users(user_uuid),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
